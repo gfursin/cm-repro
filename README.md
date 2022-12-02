30b659bb0b7ffd1ee037afb8275d93a1b0610c04
Grigori Fursin <Grigori.Fursin@cTuning.org>
2022-12-02 10:12:31 +0100

    clean up

--- README.md
+++
@@ -1,8 +0,0 @@
-# About
-
-This [CM repository](https://github.com/mlcommons/ck] contains CM scripts and workflows 
-to reproduce research projects.
-
-# Maintainers
-
-* [Grigori Fursin](https://cknowledge.io/@gfursin) (OctoML, MLCommons, cTuning foundation)
--- script/app-generate-image-dalle-mini-jax-py/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm generated-image-*.jpg
-
-${CM_PYTHON_BIN_WITH_PATH} -m pip install -r ${CM_TMP_CURRENT_SCRIPT_PATH}/requirements.txt
-test $? -eq 0 || exit 1
-
-${CM_PYTHON_BIN_WITH_PATH} ${CM_TMP_CURRENT_SCRIPT_PATH}/src/generate.py
-test $? -eq 0 || exit 1
